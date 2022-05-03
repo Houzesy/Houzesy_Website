3996ccf5f8fbd02dffdf1ded38d57d6798637f55
Jatin Chauhan <anuragmajumdar332@gmail.com>
2022-05-03 12:41:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import Navbar from './components/Navbar'
+import Footer1 from './components/Footer1'
+
+
+export class App extends Component {
+  render() {
+    return (
+      <>
+<Navbar />
+
+ <Footer1 />
+
+      </>
+    )
+  }
 }
 
-export default App;
+export default App
